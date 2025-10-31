e33f9e0dc1ae692052782d2e8f1f0230f06da773
Darsh Prajapati <darshprajapati1510@gmail.com>
2025-10-31 21:27:23 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
-// frontend/src/App.jsx
 import React, { useEffect } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 
-import SignUp from "../pages/SignUp";
+// Sahi paths (./pages/...)
+import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import ForgotPassword from "./pages/ForgotPassword";
 import Home from "./pages/Home";
 
 import { useDispatch, useSelector } from "react-redux";
 
+// Sahi paths (./hooks/...)
 import getCurrentUser from "./hooks/getCurrentUser";
 import getSuggestedUsers from "./hooks/getSuggestedUsers";
 import getAllPost from "./hooks/getAllPost";
@@ -18,6 +19,7 @@ import getFollowingList from "./hooks/getFollowingList";
 import getPrevChatUsers from "./hooks/getPrevChatUsers";
 import getAllNotifications from "./hooks/getAllNotifications";
 
+// Sahi paths (./pages/...)
 import Profile from "./pages/Profile";
 import EditProfile from "./pages/EditProfile";
 import Upload from "./pages/Upload";
@@ -30,26 +32,42 @@ import Notifications from "./pages/Notifications";
 
 import Admin from "./pages/Admin";
 import AdminPosts from "./pages/AdminPostPanel";
-import AdminLoopPanel from "./pages/AdminLoopPanel";  // <-- Import new admin loop panel
+import AdminLoopPanel from "./pages/AdminLoopPanel"; // <-- Import new admin loop panel
 
 import { io } from "socket.io-client";
+// Sahi paths (./redux/...)
 import { setOnlineUsers } from "./redux/socketSlice";
 import { setNotificationData } from "./redux/userSlice";
 
-export const serverUrl = "https://campuslink-backend-rw6d.onrender.com";
+// --- YEH AAPKA PEHLA FIX HAI ---
+// URL ko dynamic banaya gaya hai
+export const serverUrl =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:8000"
+    : "https://campuslink-backend-rw6d.onrender.com"; // Aapka deployed backend URL
 
 function App() {
+  // --- YEH AAPKA DOOSRA FIX HAI ---
+  // Hooks ko useEffect ke andar daala gaya hai taaki woh har render par na chalein
+
+  // getCurrentUser ko pehle call karein taaki user data load ho sake
   getCurrentUser();
-  getSuggestedUsers();
-  getAllPost();
-  getAllLoops();
-  getAllStories();
-  getFollowingList();
-  getPrevChatUsers();
-  getAllNotifications();
 
   const { userData, notificationData } = useSelector((state) => state.user);
   const dispatch = useDispatch();
+
+  // Baaki saare data ko tab fetch karein jab userData aa jaaye
+  useEffect(() => {
+    if (userData) {
+      getSuggestedUsers();
+      getAllPost();
+      getAllLoops();
+      getAllStories();
+      getFollowingList();
+      getPrevChatUsers();
+      getAllNotifications();
+    }
+  }, [userData]); // Yeh sirf tab chalega jab userData badlega
 
   useEffect(() => {
     if (userData) {
@@ -154,9 +172,10 @@ function App() {
       <Route
         path="/loops"
         element={userData ? <Loops /> : <Navigate to={"/signin"} />}
-      />
+      S/>
     </Routes>
   );
 }
 
 export default App;
+
