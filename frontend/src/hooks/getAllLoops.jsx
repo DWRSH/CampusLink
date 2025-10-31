0ad7d4595ac47593b9f8cbb4ff771c0d76637037
Darsh Prajapati <darshprajapati1510@gmail.com>
2025-10-31 21:44:04 +0530

    Update getAllLoops.jsx

--- frontend/src/hooks/getAllLoops.jsx
+++ frontend/src/hooks/getAllLoops.jsx
@@ -1,26 +1,21 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
-import { serverUrl } from '../App'
-import { useDispatch, useSelector } from 'react-redux'
-import { setUserData } from '../redux/userSlice'
-import { setPostData } from '../redux/postSlice'
-import { setLoopData } from '../redux/loopSlice'
+import axios from 'axios';
+import { serverUrl } from '../App'; // serverUrl ko App.jsx se import karein
+import { setLoopData } from '../redux/loopSlice';
 
-function getAllLoops() {
-    const dispatch=useDispatch()
-    const {userData}=useSelector(state=>state.user)
-   
-  useEffect(()=>{
-const fetchloops=async ()=>{
-    try {
-        const result=await axios.get(`${serverUrl}/api/loop/getAll`,{withCredentials:true})
-         dispatch(setLoopData(result.data))
-    } catch (error) {
-        console.log(error)
-    }
-}
-fetchloops()
-  },[dispatch,userData])
-}
+// --- YEH HAI SAHI TAREKA ---
+// Yeh ab ek simple async function hai, React component nahin.
+// Yeh 'dispatch' ko argument ke roop mein leta hai.
+const getAllLoops = async (dispatch) => {
+  // Humne yahan se useDispatch(), useSelector(), aur useEffect() hata diya hai.
+  try {
+    const result = await axios.get(`${serverUrl}/api/loop/getAll`, {
+      withCredentials: true,
+    });
+    // Yeh seedha us 'dispatch' function ko istemaal karta hai jo App.jsx se pass hua hai.
+    dispatch(setLoopData(result.data));
+  } catch (error) {
+    console.log("Error fetching loops:", error);
+  }
+};
 
-export default getAllLoops
+export default getAllLoops;
