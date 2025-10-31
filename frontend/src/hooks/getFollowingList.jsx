9af2f2919e97090246b9f93630070c794f733f6f
Darsh Prajapati <darshprajapati1510@gmail.com>
2025-10-31 21:47:44 +0530

    Update getFollowingList.jsx

--- frontend/src/hooks/getFollowingList.jsx
+++ frontend/src/hooks/getFollowingList.jsx
@@ -1,24 +1,21 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
-import { serverUrl } from '../App'
-import { useDispatch, useSelector } from 'react-redux'
-import { setFollowing, setUserData } from '../redux/userSlice'
-import { setCurrentUserStory } from '../redux/storySlice'
+import axios from 'axios';
+import { serverUrl } from '../App';
+// 'setUserData' aur 'setCurrentUserStory' yahaan istemaal nahin ho rahe the, isliye hata diya.
+import { setFollowing } from '../redux/userSlice';
 
-function getFollowingList() {
-    const dispatch=useDispatch()
-    const {storyData}=useSelector(state=>state.story)
-  useEffect(()=>{
-const fetchUser=async ()=>{
-    try {
-        const result=await axios.get(`${serverUrl}/api/user/followingList`,{withCredentials:true})
-         dispatch(setFollowing(result.data))
-    } catch (error) {
-        console.log(error)
-    }
-}
-fetchUser()
-  },[storyData])
-}
+// --- YEH HAI SAHI TAREKA ---
+// React, useEffect, useDispatch, useSelector ko hata diya gaya hai.
+// Function ab 'dispatch' ko seedha argument mein leta hai.
+const getFollowingList = async (dispatch) => {
+  try {
+    const result = await axios.get(`${serverUrl}/api/user/followingList`, {
+      withCredentials: true,
+    });
+    // Yeh seedha App.jsx se pass huye 'dispatch' ko istemaal karta hai.
+    dispatch(setFollowing(result.data));
+  } catch (error) {
+    console.log("Error fetching following list:", error);
+  }
+};
 
-export default getFollowingList
+export default getFollowingList;
